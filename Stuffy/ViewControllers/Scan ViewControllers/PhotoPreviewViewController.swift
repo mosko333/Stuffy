85da5ac83a70e00c47294b6321026ee79a12c9af
AdamAM <adamam@gmail.com>
2018-07-03 17:59:25 -0600

    Added Scan Storyboard and VC's

---
+++ Stuffy/ViewControllers/Scan ViewControllers/PhotoPreviewViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PhotoPreviewViewController.swift
+//  Stuffy
+//
+//  Created by Adam on 03/07/2018.
+//  Copyright © 2018 Adam Moskovich. All rights reserved.
+//
+
+import UIKit
+
+class PhotoPreviewViewController: UIViewController {
+
+    //////////////////////
+    // TODO: Replace With Model
+    //////////////////////
+    var photo: UIImage?
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateView()
+    }
+    @IBAction func saveBtnTapped(_ sender: UIBarButtonItem) {
+    }
+    
+    func updateView() {
+        //////////////////////
+        // TODO: Replace
+        //////////////////////
+        DispatchQueue.main.async {
+            self.photoImageView.image = self.photo
+        }
+    }
+    
+}
