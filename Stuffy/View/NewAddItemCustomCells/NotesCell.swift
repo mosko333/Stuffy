2411848542c9ff590abf8e16340829dd8859c668
Hayden <haydenmurdock18@gmail.com>
2018-07-18 18:34:05 -0600

    fixes add item controller stablility

--- Stuffy/View/NewAddItemCustomCells/NotesCell.swift
+++ Stuffy/View/NewAddItemCustomCells/NotesCell.swift
@@ -18,19 +18,46 @@ protocol  deleteItemDelegate: class {
 class NotesCell: UITableViewCell {
 
   
+    @IBOutlet weak var addNotesLabel: UILabel!
     @IBOutlet weak var notesTextView: UITextView!
+    @IBOutlet weak var deleteButton: UIButton!
+    var item: ItemCD?
     
     weak var delegate: deleteItemDelegate?
     
     @IBAction func deleteItemButtonTappped(_ sender: UIButton) {
         
         delegate?.deleteItem(self)
-        
     }
     
     func addDoneButton() {
         notesTextView.addDoneButtonOnKeyboard()
     
     }
+    func setupNotesCell() {
+        if item != nil {
+        deleteButton.isHidden = false
+        deleteButton.isUserInteractionEnabled = true
+        } else {
+        deleteButton.isHidden = true
+        deleteButton.isUserInteractionEnabled = false
+        }
+        
+        let placeHolderText = "Add Notes"
+        let attributedText = NSMutableAttributedString(string: placeHolderText)
+        
+        attributedText.addAttributes([NSAttributedStringKey.foregroundColor:Colors.stuffyLightGray, NSAttributedStringKey.font: UIFont(name: "Avenir-Roman", size: 16)!], range: getRangeOfSubString(subString: "Add Notes", fromString: placeHolderText))
+        
+        
+        notesTextView.attributedText = attributedText
     
+    }
+    
+    func getRangeOfSubString(subString: String, fromString: String) -> NSRange {
+        let sampleLinkRange = fromString.range(of: subString)!
+        let startPos = fromString.distance(from: fromString.startIndex, to: sampleLinkRange.lowerBound)
+        let endPos = fromString.distance(from: fromString.startIndex, to: sampleLinkRange.upperBound)
+        let linkRange = NSMakeRange(startPos, endPos - startPos)
+        return linkRange
+    }
 }
